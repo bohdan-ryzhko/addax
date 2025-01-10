f56e0037ce24e3b3a870380abdc72e6651a2fa6d
bohdan-ryzhko <ryzhkobohdan@gmail.com>
2025-01-10 17:42:03 +0200

    chore: observe change country and year

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useAppDispatch, useReduxStore } from './hooks';
 import { fetchAvailableCountries, fetchPublicHolidays } from './redux';
 import { Calendar } from './components';
@@ -6,19 +6,32 @@ import { Calendar } from './components';
 function App() {
   const dispatch = useAppDispatch();
   const { calendar, countries } = useReduxStore();
+  const previousYearRef = useRef<number | null>(null);
+  const previousCountryRef = useRef<string | null>(null);
 
   useEffect(() => {
+    if (countries.data.length > 0) return;
+
     dispatch(fetchAvailableCountries());
-  }, [dispatch]);
+  }, [countries.data.length, dispatch]);
 
   useEffect(() => {
-    if (countries.selectedCountry) {
+    const currentYear = calendar.currentMonth.getFullYear();
+    const currentCountryCode = countries.selectedCountry?.countryCode || null;
+
+    if (
+      currentCountryCode &&
+      (currentYear !== previousYearRef.current || currentCountryCode !== previousCountryRef.current)
+    ) {
       dispatch(
         fetchPublicHolidays({
-          year: calendar.currentMonth.getFullYear(),
-          countryCode: countries.selectedCountry.countryCode,
+          year: currentYear,
+          countryCode: currentCountryCode,
         }),
       );
+
+      previousYearRef.current = currentYear;
+      previousCountryRef.current = currentCountryCode;
     }
   }, [calendar.currentMonth, countries.selectedCountry, dispatch]);
 
