cfe88869dd2852ea20924bd1a36e33ff67cd3978
MaksymMV <maslakovmaksym243@gmail.com>
2025-01-18 14:07:44 +0200

    chore: update versel json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: process.env.PORT as unknown as number,
-  },
-  base: '/',
   plugins: [react()],
 });
